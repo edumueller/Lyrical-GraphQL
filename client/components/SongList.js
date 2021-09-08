caede8fe8c9989c4df9d29f6a88b2048bd81c48d
edumueller <eduardo.nicastro@icloud.com>
2021-09-07 22:48:29 -0300

    add song list apollo client query

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map((song) => (
+      <li className="collection-item" key={song.id}>
+        {song.title}
+      </li>
+    ));
+  }
   render() {
-    return <div>SongList</div>;
+    if (this.props.data.loading) return <div>Loading...</div>;
+    return <ul className="collection">{this.renderSongs()}</ul>;
   }
 }
 
-export default SongList;
+const query = gql`
+  {
+    songs {
+      id
+      title
+    }
+  }
+`;
+
+export default graphql(query)(SongList);

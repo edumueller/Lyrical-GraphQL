f71de2e1fa4005ca7803c955ccc33616d1c82caf
edumueller <eduardo.nicastro@icloud.com>
2021-09-08 14:02:03 -0300

    add lyrics list to song detail page

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -3,14 +3,19 @@ import { graphql } from 'react-apollo';
 import fetchSong from '../queries/fetchSong';
 import { Link } from 'react-router';
 import LyricCreate from './LyricCreate';
+import LyricList from './LyricList';
 
 class SongDetail extends Component {
   render() {
     const { song } = this.props.data;
+    if (!song) {
+      return <h3>Loading...</h3>;
+    }
     return (
       <div>
         <Link to="/">Back</Link>
-        <h3>{song ? song.title : 'Loading...'}</h3>
+        <h3>{song.title}</h3>
+        <LyricList lyrics={song.lyrics} />
         <LyricCreate songId={this.props.params.id} />
       </div>
     );

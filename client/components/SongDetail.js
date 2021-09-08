85eb1d3be68531c8527bbdfd27b663a5ec7ba2aa
edumueller <eduardo.nicastro@icloud.com>
2021-09-08 09:42:35 -0300

    show song title in song detail component

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import fetchSong from '../queries/fetchSong';
 
 class SongDetail extends Component {
   render() {
+    const { song } = this.props.data;
+    if (!song) {
+      return (
+        <div>
+          <h3>Loading...</h3>
+        </div>
+      );
+    }
     return (
       <div>
-        <h3>Song Detail</h3>
+        <h3>{song.title}</h3>
       </div>
     );
   }
 }
-export default SongDetail;
+export default graphql(fetchSong, {
+  // graphql intercepts the props
+  options: (props) => {
+    return { variables: { id: props.params.id } };
+  },
+})(SongDetail);

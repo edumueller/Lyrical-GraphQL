8b8d329cf067cb197fc5f6a056ad15fca2c72a13
edumueller <eduardo.nicastro@icloud.com>
2021-09-08 14:48:09 -0300

    add like icon and thumb, display ilkes

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import mutation from '../queries/likeLyric';
 
 class LyricList extends Component {
+  onLike(id) {
+    this.props.mutate({
+      variables: { id },
+    });
+  }
+
   renderLyrics() {
-    return this.props.lyrics.map(({ id, content }) => (
+    return this.props.lyrics.map(({ id, content, likes }) => (
       <li className="collection-item" key={id}>
         {content}
+        <i className="material-icons" onClick={() => this.onLike(id)}>
+          thumb_up
+        </i>
+        {likes}
       </li>
     ));
   }
@@ -13,4 +25,4 @@ class LyricList extends Component {
   }
 }
 
-export default LyricList;
+export default graphql(mutation)(LyricList);
